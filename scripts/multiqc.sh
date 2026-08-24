4e61abf7d9b0dbc0ef04d1138a6c8cc3a6e6542b
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2026-08-24 17:14:05 -0500

    Update multiqc.sh

--- scripts/multiqc.sh
+++ scripts/multiqc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -D /scratch2/nvollmer/log/array_jobs
+#SBATCH -D home/nvollmer/GmacPopGen/log/array_jobs
 #SBATCH --mail-type=END
 #SBATCH --mail-user=nicole.vollmer@noaa.gov
 #SBATCH --partition=standard
