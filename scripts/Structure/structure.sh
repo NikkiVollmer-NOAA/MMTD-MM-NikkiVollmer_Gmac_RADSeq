0ca9a226d75fd20b81062ea95748c2cf13f59a84
Nikki Vollmer <91202160+NikkiVollmer-NOAA@users.noreply.github.com>
2026-08-28 10:34:48 -0500

    Update structure.sh

--- scripts/Structure/structure.sh
+++ scripts/Structure/structure.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=END
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
-#SBATCH -D /home/nvollmer/GmacPopoGen/structure/log
+#SBATCH -D /home/nvollmer/GmacPopoGen/log/structure
 #SBATCH --array=1-100%10
 #SBATCH --mem=16G
 #SBATCH --partition=standard
